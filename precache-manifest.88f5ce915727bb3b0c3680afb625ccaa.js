783c7952636430e461a28118a72de10f6c521f49
Amey Bhavsar <iamamey24@gmail.com>
2020-07-17 01:02:00 +0530

    Updates

---
+++ precache-manifest.88f5ce915727bb3b0c3680afb625ccaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6af5f07f0a31a76e56430b9353ab128",
+    "url": "/coursera-react-1/index.html"
+  },
+  {
+    "revision": "7882a6edf0a9f190d110",
+    "url": "/coursera-react-1/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "671b689c049feede1428",
+    "url": "/coursera-react-1/static/css/main.4f15186f.chunk.css"
+  },
+  {
+    "revision": "7882a6edf0a9f190d110",
+    "url": "/coursera-react-1/static/js/2.2cb298a2.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/coursera-react-1/static/js/2.2cb298a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671b689c049feede1428",
+    "url": "/coursera-react-1/static/js/main.3d5fb78b.chunk.js"
+  },
+  {
+    "revision": "27cda478432c88699aa8",
+    "url": "/coursera-react-1/static/js/runtime-main.70f59b5a.js"
+  }
+]);
